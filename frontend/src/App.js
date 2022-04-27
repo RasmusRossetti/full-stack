b8fae6a2e055c34a6aa0fcd4e6c9dc404f426811
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2022-04-27 22:40:51 +0200

    comment change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import Form from "./components/Form";
-import Table from "./components/Table";
-import { RefreshContext } from "./contexts/RefreshContext.js";
-import { useState } from "react";
+import Form from "./components/Form"
+import Table from "./components/Table"
+import { RefreshContext } from "./contexts/RefreshContext.js"
+import { useState } from "react"
 /**
  * App is a functional parent component
  * @author Rasmus Rossetti
@@ -11,17 +11,17 @@ function App() {
   /**
    *@type {Refresh} the global state is created here for the refresh of the table
    */
-  const [refresh, setRefresh] = useState(false);
+  const [refresh, setRefresh] = useState(false)
   return (
-    <div className='pb-5 bg-gray-900 min-h-screen'>
-      /** RefreshContext.Provider is the provider for the refresh state to all
-      the other components */
+    <div className="pb-5 bg-gray-900 min-h-screen">
+      {/* RefreshContext.Provider is the provider for the refresh state to all
+      the other components */}
       <RefreshContext.Provider value={{ refresh, setRefresh }}>
         <Form />
         <Table />
       </RefreshContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
