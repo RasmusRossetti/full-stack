3d8d1b6283d1885b3c8ff696e6665ff39aedf22d
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2022-04-27 22:50:37 +0200

    updated initial state

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,20 +1,20 @@
-import axios from "axios";
-import React, { useState, useContext } from "react";
-import { RefreshContext } from "../contexts/RefreshContext.js";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import axios from "axios"
+import React, { useState, useContext } from "react"
+import { RefreshContext } from "../contexts/RefreshContext.js"
+import { toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 /**
  * npm package for messages is configured
  */
-toast.configure();
+toast.configure()
 /**
  * creating object states for the input values
  */
 const initialState = {
   name: "",
   email: "",
-  contact: "",
-};
+  contact: "+"
+}
 /**
  *  Modal is a functional component  that displays the modal when user is editing a user
  * @author Rasmus Rossetti
@@ -26,30 +26,30 @@ const Modal = ({ setShowModal, item }) => {
   /**
    * variable hooks for containing the initialState
    */
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState(initialState)
   /**
    * destructering the variables states
    */
-  const { name, email, contact } = state;
+  const { name, email, contact } = state
   /**
    * hooks for displaying error messages
    */
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("")
   /**
    * global hooks for refreshing the table
    */
-  const { refresh, setRefresh } = useContext(RefreshContext);
+  const { refresh, setRefresh } = useContext(RefreshContext)
   /**
    * handleInputChange is an arrow function for the onchange event and putting the values in the variables
    * @param {event} e event for the values
    */
   const handleInputChange = (e) => {
-    let { name, value } = e.target;
+    let { name, value } = e.target
     setState({
       ...state,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
   /**
    * updateUser is a asynchronous Arrow function that update the user information
    * @async
@@ -58,97 +58,98 @@ const Modal = ({ setShowModal, item }) => {
    */
   const updateUser = async (id) => {
     if (!name || !email || !contact) {
-      toast.error("Please provide value in each input field");
-      setErrorMessage("*");
-      return;
+      toast.error("Please provide value in each input field")
+      setErrorMessage("*")
+      return
     } else {
       const response = await axios.put(
         `http://localhost:5000/user/${id}`,
         state
-      );
+      )
       if (response.status === 200) {
-        setRefresh(!refresh);
-        toast.dark(response.data);
+        setRefresh(!refresh)
+        toast.dark(response.data)
       }
     }
-  };
+  }
   /**
    * handleSubmit is a arrow function that handles the onSubmit request in the form
    * @param {event} e event to prevent default action
    */
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     // updateUser();
-  };
+  }
 
   return (
     <>
-      <div className=' justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-        <div className='xs:w-5/6 sm:w-2/3 w-1/3  relative w-4/5 w-auto my-6 mx-auto max-w-3xl'>
-          <div className=' bg-gray-600 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-            <div className='bg-gray-600 flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
-              <h3 className='text-3xl text-gray-300 font-semibold'>
+      <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="xs:w-5/6 sm:w-2/3 w-1/3  relative w-4/5 w-auto my-6 mx-auto max-w-3xl">
+          <div className=" bg-gray-600 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="bg-gray-600 flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl text-gray-300 font-semibold">
                 Edit user
               </h3>
             </div>
 
-            <div className='bg-gray-600  p-6 flex-auto'>
+            <div className="bg-gray-600  p-6 flex-auto">
               <form
                 onSubmit={handleSubmit}
-                className='bg-gray-600 w-10/12 m-auto'
+                className="bg-gray-600 w-10/12 m-auto"
               >
                 <label
-                  htmlFor='name'
-                  className=' text-sm font-medium text-gray-900 dark:text-gray-300'
+                  htmlFor="name"
+                  className=" text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   Name
                 </label>
                 <input
-                  className='m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  type='text'
-                  id='name'
-                  name='name'
+                  className="m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="name"
+                  name="name"
                   placeholder={item.name}
                   onChange={handleInputChange}
                   value={name}
                 />
 
-                <p className='pl-3 text-red-700'>{errorMessage}</p>
+                <p className="pl-3 text-red-700">{errorMessage}</p>
                 <label
-                  htmlFor='email'
-                  className='  text-sm font-medium text-gray-900 dark:text-gray-300'
+                  htmlFor="email"
+                  className="  text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   Email
                 </label>
                 <input
-                  className='m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  type='email'
-                  id='email'
-                  name='email'
+                  className="m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="email"
+                  id="email"
+                  name="email"
                   placeholder={item.email}
                   onChange={handleInputChange}
                   value={email}
                 />
-                <p className='pl-3 text-red-700'>{errorMessage}</p>
+                <p className="pl-3 text-red-700">{errorMessage}</p>
                 <label
-                  htmlFor='contact'
-                  className=' text-sm font-medium text-gray-900 dark:text-gray-300'
+                  htmlFor="contact"
+                  className=" text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   Contact
                 </label>
                 <input
-                  className='m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  id='contact'
-                  name='contact'
+                  className="m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="contact"
+                  name="contact"
+                  type="number"
                   placeholder={item.contact}
                   onChange={handleInputChange}
                   value={contact}
                 />
-                <p className=' pl-3 text-red-700'>{errorMessage}</p>
+                <p className=" pl-3 text-red-700">{errorMessage}</p>
                 <button
                   onClick={() => updateUser(item.id)}
-                  type='submit'
-                  className='m-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 '
+                  type="submit"
+                  className="m-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 "
                 >
                   {" "}
                   Save changes
@@ -156,10 +157,10 @@ const Modal = ({ setShowModal, item }) => {
               </form>
             </div>
             {/*footer*/}
-            <div className='bg-gray-600 flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+            <div className="bg-gray-600 flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
               <button
-                className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                type='button'
+                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
                 onClick={() => setShowModal(false)}
               >
                 Close
@@ -168,9 +169,9 @@ const Modal = ({ setShowModal, item }) => {
           </div>
         </div>
       </div>
-      <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
     </>
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
